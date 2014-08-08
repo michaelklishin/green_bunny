92c4bb354a2758b95ecb800f705733c3a2ba9fee
Michael Klishin <michael@clojurewerkz.org>
2014-08-08 10:18:26 +0400

    Commit missing file

---
+++ src/main/groovy/green/bunny/GreenBunny.groovy
@@ -0,0 +1,30 @@
+package green.bunny
+
+import com.rabbitmq.client.ConnectionFactory
+
+class GreenBunny {
+  static Connection connect() {
+    def cf = new ConnectionFactory()
+    new Connection(cf, cf.newConnection())
+  }
+
+  static Connection connect(Map opts) {
+    def cf = connectionFactoryFor(opts)
+
+    new Connection(cf, cf.newConnection())
+  }
+
+  static ConnectionFactory connectionFactoryFor(Map map) {
+    def ConnectionFactory cf = new ConnectionFactory()
+
+    cf.host        = map.get("hostname") ?: (map.get("host") ?: ConnectionFactory.DEFAULT_HOST)
+    cf.port        = (map.get("port")     ?: ConnectionFactory.DEFAULT_AMQP_PORT) as Integer
+    cf.username    = map.get("username") ?: ConnectionFactory.DEFAULT_USER
+    cf.password    = map.get("password") ?: ConnectionFactory.DEFAULT_PASS
+    cf.virtualHost = map.get("vhost")    ?: ConnectionFactory.DEFAULT_VHOST
+
+    cf.requestedHeartbeat = (map.get("requested_heartbeat") ?: ConnectionFactory.DEFAULT_HEARTBEAT) as Integer
+
+    cf
+  }
+}
